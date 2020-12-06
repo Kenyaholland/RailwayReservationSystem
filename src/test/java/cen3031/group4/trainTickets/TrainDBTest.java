58f4b4295de86bbe573739a9f1f3e816cfcac121
Dylan Morris <drm54@students.uwf.edu>
2020-12-06 00:27:18 -0600

    Added Juint dependency

---
+++ src/test/java/cen3031/group4/trainTickets/TrainDBTest.java
@@ -0,0 +1,14 @@
+package cen3031.group4.trainTickets;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TrainDBTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
